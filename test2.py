9bed7d2cf5c325cebd33bf3741011c619ac3f01a
zixiao_bios <zixiao_bios@outlook.com>
2025-02-26 01:20:55 +0800

    7b运行脚本

--- test2.py
+++ test2.py
@@ -4,22 +4,14 @@ from PIL import Image
 import torch
 from transformers import AutoProcessor, LlavaForConditionalGeneration
 
-model_path = "/mnt/workspace/llava-interleave-qwen-0.5b-hf"
+model_id = "llava-hf/llava-1.5-7b-hf"
 model = LlavaForConditionalGeneration.from_pretrained(
-    model_path, 
+    model_id, 
     torch_dtype=torch.float16, 
     low_cpu_mem_usage=True, 
 ).to(0)
 
-# print("================= model =================")
-# print(f"{model}")
-
-# print("================= named_modules =================")
-# for each in model.named_modules():
-#     print(f"{each}")
-
-
-processor = AutoProcessor.from_pretrained(model_path)
+processor = AutoProcessor.from_pretrained(model_id)
 
 # Define a chat history and use `apply_chat_template` to get correctly formatted prompt
 # Each value in "content" has to be a list of dicts with types ("text", "image") 
@@ -28,7 +20,7 @@ conversation = [
 
       "role": "user",
       "content": [
-          {"type": "text", "text": "这里有什么?写一首诗"},
+          {"type": "text", "text": "What are these?"},
           {"type": "image"},
         ],
     },
