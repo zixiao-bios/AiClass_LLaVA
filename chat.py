256a1f7cb20190d5ef105cc13e8051559b771680
zixiao_bios <zixiao_bios@outlook.com>
2025-03-05 14:23:07 +0800

    chat支持lora

--- chat.py
+++ chat.py
@@ -2,10 +2,12 @@ import requests
 from PIL import Image
 import torch
 from transformers import AutoProcessor, LlavaForConditionalGeneration, BitsAndBytesConfig
+from peft import PeftModel
 
 # 本地模型路径（需替换为实际路径）
-model_path = "/mnt/workspace/llava-1.5-7b-hf"
-# model_path = "/mnt/workspace/lora_llava_finetuned"
+# model_path = "/mnt/workspace/llava-1.5-7b-hf"
+model_path = "/mnt/workspace/llava-interleave-qwen-0.5b-hf"
+lora_path = "/mnt/workspace/lora_llava_finetuned"
 
 # 配置量化参数
 bnb_config = BitsAndBytesConfig(
@@ -23,11 +25,14 @@ model = LlavaForConditionalGeneration.from_pretrained(
     model_path,
     torch_dtype=torch.float16,
     low_cpu_mem_usage=True,
-    quantization_config=bnb_config,
+    # quantization_config=bnb_config,
 )
 
+if lora_path:
+    model = PeftModel.from_pretrained(model, lora_path)
+
 # 使用量化时，模型会自动传到 GPU，不支持手动指定
-# model = model.to(0)
+model = model.to(0)
 model.eval()
 
 # 加载处理器参数说明：
